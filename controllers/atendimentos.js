2a5cbb6edcb3335b48c61d017ada3a700c98b5fe
walace <walacefex01@gmail.com>
2021-01-03 12:23:56 -0300

    criando as rotas de teste para utilizar no postman

---
+++ controllers/atendimentos.js
@@ -0,0 +1,7 @@
+module.exports = app =>{
+  app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos GET'));
+  app.post('/atendimentos', (req,res) => {
+    console.log(req.body)
+    res.send('Você está na rota de atendimentos POST') 
+  })
+}
